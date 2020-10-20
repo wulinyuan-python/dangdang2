e853f032ed048e1b7c2de0b14bf37d3ab3793659
wulinyuan <8202453+wulinyuan@user.noreply.gitee.com>
2020-10-20 10:02:54 +0800

    增加登陆函数

--- user/views.py
+++ user/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
     print('这是首页')
     return HttpResponse('hehe')
+
+def login(request):
+    print('giao')
+    return HttpResponse('登陆')
